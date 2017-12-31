0536725bfac44d3daaf197fbb51a6f5a29f978f2
pfwu <acmlxg@163.com>
2017-12-31 17:24:05 +0800

    add tensorboard in tensorflow/NN 2017/12/29

--- tensorflow/NN/Init_A_NN.py
+++ tensorflow/NN/Init_A_NN.py
@@ -10,6 +10,7 @@ Created on  2017/12/29 0029 22:49
 
 import tensorflow as tf
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 
 
@@ -25,43 +26,57 @@ def add_layer(inputs,in_size,out_size,activation_function=None):   #默认无激
 
 
 x_data = np.linspace(-1,1,300)[:,np.newaxis]  #?????
-np.random.seed(1)
+# x_data = np.linspace(-np.pi,np.pi,300)[:,np.newaxis]
+np.random.seed(32)
+
 noise = np.random.normal(0,0.05,x_data.shape)
-y_data = np.square(x_data) - 0.5
+y_data = np.square(x_data) - + noise
+# y_data = np.sin(x_data) - +noise
 
 
-xs = tf.placeholder(tf.float32,[None,1]) #None 无论给多少个例子都行，后面的1是x的维度
+xs = tf.placeholder(tf.float32,[None,1])  # None 无论给多少个例子都行，后面的1是x的维度
 ys = tf.placeholder(tf.float32,[None,1])
 
-hidden_layer_out = add_layer(xs,1,10,activation_function=tf.nn.relu)  #隐藏层10个神经元
-prediction = add_layer(hidden_layer_out,10,1,activation_function=None)
-
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1])) #求和后求平均
-
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
-
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.scatter(x_data,y_data)
-plt.ion()    #加上这个之后，相当于holdon
+# hidden_cells = 10
 
 
-for i in range(1000):
-    sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
-    if i%50 == 0:
-        print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
-        try:
-            ax.lines.remove(lines[0])  # 如果要求连续效果，必须抹除上次的线
-        except Exception as e:
-            pass
-        prediction_value = sess.run(prediction,feed_dict={xs:x_data})
-        lines = ax.plot(x_data,prediction_value,'r-',lw=5)
-        plt.pause(0.1)
+def Train(hidden_cells):
+    hidden_layer_out = add_layer(xs,1,hidden_cells,activation_function=tf.nn.relu)  #隐藏层10个神经元
+    prediction = add_layer(hidden_layer_out,hidden_cells,1,activation_function=None)
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]))  #求和后求平均
+    # train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+    train_step = tf.train.AdamOptimizer(0.1).minimize(loss)
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+    for i in range(1000):
+        sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
+        # if i%50 == 0:
+        #     # print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+        #     try:
+        #         ax.lines.remove(lines[0])  # 如果要求连续效果，必须抹除上次的线
+        #     except Exception as e:
+        #         pass
+        #     prediction_value = sess.run(prediction,feed_dict={xs:x_data})
+        #     lines = ax.plot(x_data,prediction_value,'r-',lw=5)
+        #     plt.ylim((y_data.min() * 1.1, y_data.max() * 1.1))
+        #     plt.pause(0.1)
+    return sess.run(loss,feed_dict={xs:x_data,ys:y_data})
 
 
-plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(1,1,1)
+# ax.scatter(x_data,y_data)
+# plt.ion()    #加上这个之后，相当于holdon
+
+min_loss=[]
+if __name__ == '__main__':
+    start = time.time()
+    with open('log.log','a') as f:
+        for j in range(1,101):
+            min_loss.append(Train(j))
+            print('Epoch %d loss : %f'%(j,min_loss[j-1]),file=f)
+        print('The loss min is :%f,in Epoch %dth'%(min(min_loss),(min_loss.index(min(min_loss)) + 1)),file=f)
+        print('Time:%f seconds'%(time.time()-start),file=f)
+    # plt.show()
 
